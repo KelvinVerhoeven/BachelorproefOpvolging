3df9788b47f8b7bd6a4a0cefb87be5e021d3f3f4
jonathan2266 <jonathan2266@hotmail.be>
2016-10-16 17:31:12 +0200

    nodejs git connection feature

--- src/BAPsync/app.js
+++ src/BAPsync/app.js
@@ -1,61 +1,58 @@
 ﻿var express = require('express');
+var fs = require("fs");
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var cookieparser = require('cookie-parser');
+var GitHubApi = require("github");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+console.log('required everything');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+//variables
+var token = "";
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+
+var github = new GitHubApi({
+    debug: true,
+    protocol: "https",
+    host: "api.github.com",
+    headers: {
+        "user-agent": "automat-BAP"
+    },
+    Promise: require('bluebird'),
+    followRedirects: false,
+    timeout: 5000
 });
 
-// error handlers
+var authenticateUser = function () {
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+    token = fs.readFileSync("key.txt").toString();
+
+    github.authenticate({
+        type: "token",
+        token: token,
     });
+
+    github.users.getFollowingForUser({
+        user: "jonathan2266"
+    }, function (err, res) {
+        console.log(JSON.stringify(res));
+    });
+
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+
+
+
+
+
+
+app.listen(process.env.NODEJS_PORT || 3541, function () {
+    console.log('Server running ' + (process.env.NODEJS_PORT || 3541));
 });
 
-
-module.exports = app;
+authenticateUser();
