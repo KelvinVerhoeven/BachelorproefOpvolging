811e480ad24eedc27c3523560d97c3068ebe3fa5
jonathan2266 <jonathan2266@hotmail.be>
2016-11-20 12:31:08 +0100

    finished issuesForm feature

--- src/BAPsync/public/javascripts/issuesFormApp.js
+++ src/BAPsync/public/javascripts/issuesFormApp.js
@@ -2,4 +2,39 @@
 
 app.controller("IssuesFormCtrl", ["$cookies", "$scope", "$http", "$window", function ($cookies, $scope, $http, $window) {
 
+
+    $scope.submitIssue = function (title, text) {
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        var dataToSend = { username: $cookies.get("username"), password: $cookies.get("password"), student: $cookies.get("currentStudent"), repo: $cookies.get("currentRepo"), title: title, body: text };
+
+        $http.post("/issues/create", dataToSend, config)
+            .success(function (data, status, headers, config) {
+                if (data.done == true) {
+                    $window.location.href = "/issues";
+                }
+            })
+            .error(function (data, status, header, config) {
+                console.log("Failed " + data);
+                confirm("update failed");
+            })
+    }
+
+    var init = function () {
+
+        if ($cookies.get("currentStudent") == undefined || $cookies.get("currentRepo") == undefined) {
+            $window.location.href = "/overview";
+        }
+        if ($cookies.get("username") == undefined || $cookies.get("password") == undefined) {
+            $window.location.href = "/login";
+        }
+
+
+    }
+
+    init();
 }]);
