bedc5b1f904f46060f90e3caf73a1b995aa8cc81
KelvinVerhoeven <kelvin.verhoeven@gmail.com>
2016-11-21 11:46:58 +0100

    page automatically show first student in array on load

--- src/BAPsync/public/javascripts/OverviewApp.js
+++ src/BAPsync/public/javascripts/OverviewApp.js
@@ -5,12 +5,30 @@ app.controller("overviewCtrl",
     "$cookies", "$scope", "$http", "$window", function($cookies, $scope, $http, $window) {
 
         $scope.students = [];
-        $scope.docent;
 
         $scope.navigation = function(link) {
             var host = $window.location.host;
             var result = "https://" + host + "/" + link;
             $window.location.href = result;
+        }
+
+        var getRepos = function () {
+            var config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            var dataToSend = {
+                username: $cookies.get("username"), password: $cookies.get("password"),
+                owner: $cookies.get("currentStudent"), repo: $cookies.get("currentRepo")
+            };
+            $http.post("/repos/get", dataToSend, config)
+                .success(function (data, status, header, config) {
+                    $scope.repolink = data.html_url;
+                })
+                .error(function (data, status, header, config) {
+                    console.log("Failed! " + data);
+                });
         }
 
         $scope.chooseStudent = function(full) {
@@ -38,24 +56,32 @@ app.controller("overviewCtrl",
             
         }
 
-        var getRepos = function() {
+        var chooseStudent = function (full) {
             var config = {
                 headers: {
                     'Content-Type': 'application/json'
                 }
             }
             var dataToSend = {
-                username: $cookies.get("username"), password: $cookies.get("password"),
-                owner: $cookies.get("currentStudent"), repo:$cookies.get("currentRepo")
+                "full": full
             };
-            $http.post("/repos/get", dataToSend, config)
-                .success(function(data, status, header, config) {
-                    $scope.repolink = data.html_url;
+
+            $http.post("/overview/get", dataToSend, config)
+                .success(function (data, status, header, config) {
+                    $scope.name = data.owner;
+                    $cookies.put("currentStudent", data.owner, ["secure", "true"]);
+                    $cookies.put("currentRepo", data.repo, ["secure", "true"]);
+                    getRepos();
+
                 })
-                .error(function(data, status, header, config) {
+                .error(function (data, status, header, config) {
                     console.log("Failed! " + data);
                 });
+
+
         }
+
+        
 
 
         var init = function () {
@@ -77,23 +103,32 @@ app.controller("overviewCtrl",
             $http.post("/subscriptionList", dataToSend, config)
                 .success(function (data, status, headers, config) {
                     $scope.students = data;
+                    chooseStudent($scope.students[0].studentRepo);
                 })
                 .error(function (data, status, header, config) {
                     console.log("Failed " + data);
                 });
 
+            
+            
+
             /*var dataToSend2 = {
-                username: $cookies.get("username"), password: $cookies.get("password"),
-                owner: $cookies.get("currentStudent"), repo: $cookies.get("currentRepo")
+                "full": full
             };
 
-            $http.post("/repos/get", dataToSend2, config)
+            $http.post("/overview/get", dataToSend2, config)
                 .success(function (data, status, header, config) {
-                    $scope.name = data;
+                    $scope.name = data.owner;
+                    $cookies.put("currentStudent", data.owner, ["secure", "true"]);
+                    $cookies.put("currentRepo", data.repo, ["secure", "true"]);
+                    getRepos();
+
                 })
                 .error(function (data, status, header, config) {
                     console.log("Failed! " + data);
                 });*/
+
+
         }
 
         init();
